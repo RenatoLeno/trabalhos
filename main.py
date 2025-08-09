383c21a20bf2f8207853fdcbfacffb80a33b603b
RenatoLeno <renatoleno@gmail.com>
2025-08-09 17:36:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Vá até a pasta do seu projeto
+cd C:\Users\RENATO LENO\Desktop\IESB
+
+# Inicialize um repositório Git local
+git init
+
+# Adicione o arquivo main.py
+git add main.py
+
+# Faça um commit com uma mensagem
+git commit -m "Adiciona código do sistema de cadastro de produtos"
+
+# Conecte seu repositório local ao repositório remoto no GitHub
+git remote add origin https://github.com/seu-usuario/cadastro-produtos.git
+
+# Envie o código para o GitHub
+git push -u origin master
